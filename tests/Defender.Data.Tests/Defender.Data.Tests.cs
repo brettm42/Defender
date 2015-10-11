273882a9859398e7a3c9bd3808cc5d3d8be1d023
brettm42 <brettm42@gmail.com>
2015-10-10 20:08:41 -0700

    rough sketches of potential UnitTests

--- tests/Defender.Data.Tests/Defender.Data.Tests.cs
+++ tests/Defender.Data.Tests/Defender.Data.Tests.cs
@@ -6,10 +6,49 @@
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+    using Defender.Data;
+    using Defender.Model;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     [TestClass]
     public class DataTests
     {
+        [TestMethod]
+        [TestCategory("UnitTest")]
+        void SerialiseData()
+        {
+            //Serializer _serial = new Serializer(new ObservableCollection );
+
+            //string serialised = _serial.SerialiseToString();
+
+            //Assert.AreEqual(Resources/SerialisedData, serialised, "Expected encoded data to match, does not!");
+        }
+
+        [TestMethod]
+        [TestCategory("UnitTest")]
+        void DeserialiseData()
+        {
+            //Serializer _serial = new Serializer(new ObservableCollection );
+
+            //ObservableCollection deserialised = _serial.DeserialiseFromString(Resources/BinaryData);
+
+            //Assert.AreEqual(Resources/DataGrid, _serial.DataGrid, "Expected decoded data to match original data, it doesn't!");
+
+        }
+
+        [TestMethod]
+        [TestCategory("UnitTest")]
+        void SerialiseDeserialiseData()
+        {
+            //Serializer _serial = new Serializer(new ObservableCollection );
+
+            //string serialised = _serial.SerialiseToString();
+
+            //ObservableCollection deserialised = _serial.DeserialiseFromString(Resources/BinaryData);
+
+            //Assert.AreEqual(Resources/SerialisedData, serialised, "Expected encoded data to match, does not!");
+            //Assert.AreEqual(Resources/DataGrid, _serial.DataGrid, "Expected decoded data to match original data, it doesn't!");
+
+        }
     }
 }
