0d1b8855bf77a3ae655f37ba4989119338e508ef
brettm42 <brettm42@gmail.com>
2015-11-01 09:58:10 -0800

    many small fixes

--- src/Defender.Model/Defender.Model.Base.cs
+++ src/Defender.Model/Defender.Model.Base.cs
@@ -1,10 +1,9 @@
 ﻿namespace Defender.Model
 {
     using System;
-    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
     using System.Linq;
     using System.Text;
-    using System.Threading.Tasks;
 
     public static class ModelBase
     {
